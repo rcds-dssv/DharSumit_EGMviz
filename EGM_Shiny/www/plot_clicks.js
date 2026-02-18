47fcfff7a7e874769437b461730a639bb1561ae2
ageller <a-geller@northwestern.edu>
2026-02-18 14:51:48 -0600

    fixed the click marker arrow

--- EGM_Shiny/www/plot_clicks.js
+++ EGM_Shiny/www/plot_clicks.js
@@ -1,6 +1,25 @@
 // flag to track if a point was clicked (vs. background click)
 var pointClicked = false;
 
+getPlotlyPositionOnPage = function(eventData){
+    // from ChatGPT
+    var point = eventData.points[0];
+    var plot = document.getElementById("egm-egm_plot");
+
+    // get plot layout + axes
+    var xaxis = plot._fullLayout.xaxis;
+    var yaxis = plot._fullLayout.yaxis;
+
+    // data to plot pixels
+    var x_px = xaxis.l2p(point.x);
+    var y_px = yaxis.l2p(point.y);
+
+    // account for plot margins
+    var x = x_px + plot._fullLayout.xaxis._mainAxis._offset;
+    var y = y_px +  plot._fullLayout.yaxis._mainAxis._offset;
+    return {x:x, y:y}
+
+}
 handlePlotlyClicks =  function(eventData) {
     // eventData.points is an array of points clicked
     if ("points" in eventData){
@@ -9,23 +28,17 @@ handlePlotlyClicks =  function(eventData) {
             // reset the flag after a brief delay
             setTimeout(function() { pointClicked = false; }, 50);
 
-            // // Data coordinates
-            // var point = eventData.points[0];
-            // var x_data = point.x;
-            // var y_data = point.y;
-
-
-            // add the tooltip
-            var tooltip = document.getElementById('clicked_point_marker');
-            var wrapper = tooltip.parentElement;
+            // add the arrow
+            var arrow = document.getElementById('clicked_point_marker');
 
             // Convert page coordinates to container-relative
-            var wrapperBbox = wrapper.getBoundingClientRect();
-            var tooltipBbox = tooltip.getBoundingClientRect()
-            
-            tooltip.classList.remove("hidden");
-            tooltip.style.left = (eventData.event.pageX - wrapperBbox.left - window.scrollX) + 'px';
-            tooltip.style.top = (eventData.event.pageY - wrapperBbox.top - window.scrollY - tooltipBbox.height/2.) + 'px';
+            arrow.classList.remove("hidden");
+            var arrowBbox = arrow.getBoundingClientRect()
+            var pos = getPlotlyPositionOnPage(eventData)
+            var x = pos.x + 4; 
+            var y = pos.y - arrowBbox.height / 2;
+
+            arrow.setAttribute("transform", `translate(${x}, ${y})`);
 
 
 
@@ -34,9 +47,9 @@ handlePlotlyClicks =  function(eventData) {
 }
 handlePlotBackgroundClick =  function(event) {
     if (!pointClicked) {
-        // remove the tooltip
-        const tooltip = document.getElementById('clicked_point_marker');
-        tooltip.classList.add("hidden");
+        // remove the arrow
+        const arrow = document.getElementById('clicked_point_marker');
+        arrow.classList.add("hidden");
         // reset the plot colors and table
         Shiny.setInputValue("egm-reset_plot", Math.random(), {priority: "event"});
     }
@@ -52,12 +65,32 @@ function attachPlotlyClickHandler() {
             plot.on("plotly_click", handlePlotlyClicks);
             console.log("Plotly handler attached");
 
-            // create a hidden div for the tooltip to mark clicks
-            const el = document.createElement("div");
-            el.id = "clicked_point_marker";
-            el.classList.add("hidden");
-            // document.body.appendChild(el);
-            plot.querySelector(".svg-container").appendChild(el);
+            // create a hidden div for the arrow to mark clicks
+            // const el = document.createElement("div");
+            // el.id = "clicked_point_marker";
+            // el.classList.add("hidden");
+            // // document.body.appendChild(el);
+            // plot.querySelector(".svg-container").appendChild(el);
+
+            const svgs = plot.querySelectorAll(".main-svg");
+            // const svg = svgs[svgs.length - 1];
+            const svg = svgs[1];
+            var arrow = document.createElementNS("http://www.w3.org/2000/svg", "path");
+            arrow.setAttribute("d",
+                "M 0 10 " +   // tip
+                "L 16 0 " +   // top-left of head
+                "L 16 6 " +   // top of tail
+                "L 40 6 " +   // top-right of tail
+                "L 40 14 " +  // bottom-right of tail
+                "L 16 14 " +  // bottom of tail
+                "L 16 20 " +  // bottom-left of head
+                "Z"
+            );            
+            arrow.setAttribute("id", "clicked_point_marker")
+            arrow.classList.add("hidden");
+            svg.insertBefore(arrow, svg.querySelector('.infolayer'));
+            // svg.appendChild(arrow);
+            // arrow.setAttribute("transform", `translate(${x}, ${y})`);
 
             return;
         } 
